c8a277115ca3e70e6e70d7305c4ac5fb8a5133a0
Diamond Buchanan <winterdiamon@hotmail.com>
2024-12-02 00:59:13 -0500

    1:00

---
+++ XfinityTools/Pages/JS/DropDownMenu.js
@@ -0,0 +1,9 @@
+﻿// Dropdown functionality
+const dropdown = document.querySelector('.dropdown');
+const dropdownContent = document.querySelector('.dropdown-content');
+dropdown.addEventListener('mouseenter', () => {
+    dropdownContent.style.display = 'block';
+});
+dropdown.addEventListener('mouseleave', () => {
+    dropdownContent.style.display = 'none';
+});
